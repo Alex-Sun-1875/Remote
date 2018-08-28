4e7577fa3c7803b02211e66b87df203433f2bebf
sunlh <200359659@qq.com>
2018-08-28 13:31:20 +0000

    modify BUILD.gn and main.cc

--- main.cc
+++ main.cc
@@ -1,10 +1,29 @@
 #include <iostream>
 #include "base/command_line.h"
+#include "base/threading/platform_thread.h"
+
+#include "content/child/child_process.h"
+#include "content/child/child_thread_impl.h"
 
 int main(int argc, char* argv[]) {
   std::cout << "Start A Remote!!!" << std::endl;
+  base::PlatformThread::SetName("RemoteMain");
+
   base::CommandLine::Init(argc, argv);
   base::CommandLine* cmd_line = base::CommandLine::ForCurrentProcess();
+
+  cmd_line->AppendSwitchASCII("", "");
+
+  base::MessageLoop main_loop;
+
+  ChildProcess child_process;
+  ChildThread::Options::Builder builder;
+  ChildThread* child_thread = new ChildThreadImpl(builder.WithChannelName("0.0.0.0:12345")
+                                                         .WithChannelMode(ipc::Channel::MODE_NAMED_SERVER)
+                                                         .WithChannelType(ipc::Channel::TYPE_DGRAM)
+                                                         .Build());
+  child_process.set_main_thread(child_thread);
+  main_loop.Run();
 
   return 0;
 }
